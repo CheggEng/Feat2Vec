71c117ef95ba9b44ce8bc82ac7b39a3dd7717ce5
Luis Armona <larmona@chegg.com>
2017-09-14 23:03:07 +0000

    moved over oss dir from bitbucket

---
+++ test_implicitsampler.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import implicitsampler
+import itertools
+reload(implicitsampler)
+from implicitsampler import SamplingStrategy, ImplicitSampler
+from deepfm import DeepFM
+import keras
+import tensorflow as tf
+
+#create sample DF
+#that we will implicitly sample from
+
+np.random.seed(1)
+#generate some data
+samplesize = 100000
+batch_size=2000
+neg_samples=5
+embed_dim = 10
+
+
+attrs = np.random.multivariate_normal(mean = np.array([0.,0.]), cov = np.array([[1.,.1],[.1,1]]),size=samplesize)
+testdata = pd.DataFrame({ 'item': np.random.randint(0,10,size=samplesize),
+						  'id': np.random.randint(0,50,size=samplesize),
+                          'attr1':attrs[:,0],
+                          'attr2':attrs[:,1],
+                          'attr3': np.random.uniform(size=samplesize),
+						  'offset_':np.ones(samplesize)
+	})
+
+testdata['latenty'] = ( (testdata.attr3)**4/600. + np.exp(testdata.attr1*testdata.attr2) \
+                       + (testdata.item<=3)*np.pi - ((testdata.item==2) | (testdata.item==4))*testdata.attr3 \
+                       + ((testdata.item==8) | (testdata.item==7) )*testdata.attr2 \
+                       + (testdata.item>=6)*testdata.attr1 + (testdata.id%3)*testdata.attr3 \
+                       + np.random.normal(size=samplesize) - 5 )
+testdata['latenty'][np.abs(testdata['latenty']) >= 10] = 0
+testdata['y'] = np.floor(testdata.latenty)
+print testdata.y.value_counts()/samplesize
+
+model_features = [['item'],['id'],['attr1','attr2','attr3'],['y'],['offset_']]
+features = ['item','id','attrs','y','offset']
+sampling_features = model_features
+#itemset_list = [items3,items12]
+
+#build the item list myself
+reload(implicitsampler)
+from implicitsampler import SamplingStrategy, ImplicitSampler
+
+sampler = ImplicitSampler(testdata,model_features=model_features,sampling_features=sampling_features,batch_size=batch_size,
+                       sampling_alpha=.25,sampling_bias=0,oversampler='twostep',negative_samples=neg_samples)
+
+#print "----"
+#sample_strat = SamplingStrategy(features=features,items =[items3,items12] ,alpha=.5,bias=10)
+#P = sample_strat.get_probability(testdata)
+
+#ImpS = ImplicitSampler(testdata,features=features,model_features=model_features,batch_size=batch_size,
+#                       sampling_strategy=sample_strat,
+#                       sampling_features=sampling_features,sampling_items=itemset_list)
+
+#print sample_strat.get_probability(testdata)[1]
+print "^*^*^*^*^*"
+ind=1
+for x,y,w in ImpS.keras_generator():
+    ycounts= pd.Series(x[features.index('y')].flatten()).value_counts()/(batch_size*(1+neg_samples))
+    print ycounts
+    if ind >=1:
+        break
+    ind+=1
+    print "----"
+           
