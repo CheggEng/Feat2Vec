27c7df1fb6f6b65c8041d2f73ba9433cb6eecd0a
luisarmona <luisarmona@gmail.com>
2017-09-22 17:09:31 -0700

    created exploratory hists + title generation script

---
+++ paper/create_title_data.py
@@ -0,0 +1,71 @@
+#create_data.py
+#merge together IMDB title-level data
+import pandas as pd
+import numpy as np
+import gzip
+import cPickle
+import matplotlib.pyplot as plt
+datadir = '/home/luis/Downloads/'
+datadir = '/media/luis/hdd3/Data/IMDB/'
+#load data
+with gzip.open(datadir + 'title.basics.tsv.gz') as f:
+    df = pd.read_csv(f,sep='\t')
+
+#throw out some titles with tabs in it.
+df['isAdult']=df['isAdult'].astype('str')
+df = df.loc[df.isAdult.isin(['0','1'])==True,:]
+print df.head()
+
+#fill Nans with null identifier
+for c in ['titleType','isAdult','startYear','endYear','runtimeMinutes']:
+    print c
+    vcounts = df[c].value_counts(dropna=False)
+    print len(vcounts)
+    print vcounts
+
+
+#create genre matrix
+print "creating genre matrix"
+genstrMat = df['genres'].str.split(',',expand=True)
+genstrMat = np.array(genstrMat.fillna(r'\N'))
+genre_types = np.unique(genstrMat).tolist()
+for g in genre_types:
+    df[r'genre_{}'.format(g)] = np.any(genstrMat==g,axis=1)
+    df[r'genre_{}'.format(g)] = df[r'genre_{}'.format(g)].astype('bool')
+
+
+
+#import crew and principals data
+print "importing crew data"
+with gzip.open(datadir + 'title.crew.tsv.gz') as f:
+    crew = pd.read_csv(f,sep='\t')
+
+print crew.head()
+df = df.join(crew.set_index('tconst'),on='tconst')
+
+print "importing principal cast data"
+with gzip.open(datadir + 'title.principals.tsv.gz') as f:
+    princ = pd.read_csv(f,sep='\t')
+
+print princ.head()
+df = df.join(princ.set_index('tconst'),on='tconst')
+
+print "importing ratings data"
+with gzip.open(datadir + 'title.ratings.tsv.gz') as f:
+    ratings = pd.read_csv(f,sep='\t')
+print ratings.head()
+df = df.join(ratings.set_index('tconst'),on='tconst')
+#convert strings to lists
+print "list-ifying sequence vars"
+for c in ['directors','writers','principalCast']:
+    print c
+    df[c] = df[c].fillna(r'\N')
+    df[c] = df[c].str.split(',',expand=False)
+    print df[c].map(lambda x: len(x)).value_counts()
+
+
+print df.shape
+print df.head()
+print "saving to file"
+with open(os.path.join(datadir,'imdb_title_data.p'),'w') as f:
+    cPickle.dump(df,f)
--- paper/explore_data.py
+++ paper/explore_data.py
@@ -3,55 +3,75 @@
 import pandas as pd
 import numpy as np
 import gzip
+import os
 import matplotlib.pyplot as plt
 datadir = '/home/luis/Downloads/'
-#load data
+datadir = '/media/luis/hdd3/Data/IMDB/'
+outputdir = '/home/luis/feat2vec/paper/output/'
+#load titles data
 with gzip.open(datadir + 'title.basics.tsv.gz') as f:
-    df = pd.read_csv(f,sep='\t')
+    titles = pd.read_csv(f,sep='\t')
 
 #throw out some titles with tabs in it.
-df['isAdult']=df['isAdult'].astype('str')
-df = df.loc[df.isAdult.isin(['0','1'])==True,:]
-
-#create genre matrix
-genstrMat = df['genres'].str.split(',',expand=True)
-genstrMat = np.array(genstrMat.fillna(r'\N'))
-genre_types = np.unique(genstrMat).tolist()
-for g in genre_types:
-    df[r'genre_{}'.format(g)] = np.any(genstrMat==g,axis=1)
-    df[r'genre_{}'.format(g)] = df[r'genre_{}'.format(g)].astype('int8')
-
-
-
+titles['isAdult']=titles['isAdult'].astype('str')
+titles = titles.loc[titles.isAdult.isin(['0','1'])==True,:]
+print titles.head()
 
 #import crew and principals data
 with gzip.open(datadir + 'title.crew.tsv.gz') as f:
     crew = pd.read_csv(f,sep='\t')
-print crew['directors'].str.split(',',expand=True).head()
-print crew.head()
 
-df = df.join(crew.set_index('tconst'),on='tconst')
+print crew.head()
 
 with gzip.open(datadir + 'title.principals.tsv.gz') as f:
     princ = pd.read_csv(f,sep='\t')
-print princ.head()
-df = df.join(princ.set_index('tconst'),on='tconst')
 
-#princ = df['principalCast'].str.split(',',expand=True)
 print princ.head()
+
+with gzip.open(datadir + 'name.basics.tsv.gz') as f:
+    names = pd.read_csv(f,sep='\t')
+
+print names.head()
 
 
 with gzip.open(datadir + 'title.ratings.tsv.gz') as f:
     ratings = pd.read_csv(f,sep='\t')
 print ratings.head()
-df = df.join(ratings.set_index('tconst'),on='tconst')
-
-for c in ['titleType','isAdult','startYear']:
-    print df[c].value_counts()
-
-df.isAdult.value_counts()
 
 
 #plot start year of films
-plt.hist(pd.to_numeric(df.startYear,errors='coerce').dropna(),bins=100)
+startyears = pd.to_numeric(titles.startYear,errors='coerce').dropna()
+print startyears.describe()
+plt.hist(startyears,bins=100)
+plt.xlim([np.min(startyears),2017])
+plt.savefig(os.path.join(outputdir,'startYear_hist.pdf'))
+plt.show()
+
+
+#plot endyears (TV only)
+endyears = pd.to_numeric(titles.endYear,errors='coerce').dropna()
+print endyears.describe()
+plt.hist(endyears,bins=100)
+plt.xlim([np.min(endyears),2017])
+plt.savefig(os.path.join(outputdir,'endYear_hist.pdf'))
+plt.show()
+
+#plot runtime of films
+minutes =  pd.to_numeric(titles.runtimeMinutes,errors='coerce').dropna()
+print minutes.describe()
+plt.hist(minutes,bins=np.logspace(0, np.log10(np.max(minutes)),100))
+plt.xscale('log')
+plt.savefig(os.path.join(outputdir,'runTime_hist.pdf'))
+plt.show()
+
+#plot ratings of films
+plt.hist(pd.to_numeric(ratings.averageRating,errors='coerce').dropna(),bins=100)
+plt.savefig(os.path.join(outputdir,'ratings_hist.pdf'))
+plt.show()
+#plot votes of films
+votes = pd.to_numeric(ratings.numVotes,errors='coerce').dropna()
+print votes.describe()
+plt.hist(votes,bins=np.logspace(0, np.log10(np.max(votes)),100))
+plt.xscale('log')
+plt.savefig(os.path.join(outputdir,'numVotes_hist.pdf'))
 plt.show()
