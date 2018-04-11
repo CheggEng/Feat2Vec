798ed7b6c6c9fe9a4332fb78c1df4c2e9ecd5d74
luisarmona <luisarmona@gmail.com>
2018-04-11 15:31:08 -0700

    updated F2V Yelp Experiment

--- paper/create_yelp_data.py
+++ paper/create_yelp_data.py
@@ -5,64 +5,40 @@ import numpy as np
 import os
 import json
 import cPickle
-import re
+import gc
 from keras.preprocessing.text import Tokenizer
-datadir = '/home/luis/Data/Yelp/dataset/'
+import yaml
+constants = yaml.load(open("paper/constants.yaml", 'rU'))
+datadir =constants['datadir']
 
 with open(os.path.join(datadir,"review.json"),"r") as f:
-    review_json=f.read()
-parsed_jsons=[]
+    review_jsons=f.read()
+
 num_reviews=1000000
 max_vocab = 100000 #max vocab size
-for i in review_json.split("\n")[0:num_reviews]:
-#for i in review_json.split("\n"):
+#for i in review_json.split("\n")[0:num_reviews]:
+print "Splitting json file..."
+review_jsons=review_jsons.split("\n")
+print len(review_jsons)
+print "passing JSON files to pandas"
+reviews = []
+for i in review_jsons:
+#for i in review_jsons[0:num_reviews]:
     try:
-        parsed_jsons.append(json.loads(i))
+        reviews.append(json.loads(i))
     except:
         pass
-review_pd=pd.DataFrame(parsed_jsons)
-print review_pd.head()
+print "Concatenating..."
+reviews=pd.DataFrame(reviews)
+print reviews.head()
 
-#remove ambience indicators
-review_pd.drop(['cool','useful','funny','date'],axis=1,inplace=True)
-
-### Transform discrete vars to OHE
-print "Preprocessing the embeddings..."
-#vocab_map = {}
-for c in ['business_id','user_id','stars']:
-    review_pd[c] = pd.Categorical(review_pd[c])
-
-
-
-### Filter text
-print "Fitting review text data"
-review_pd['text'] = review_pd['text'].map(lambda x: re.sub( '\s+', ' ', x)) #change all whitespace to spaces
-review_filter = Tokenizer(num_words = max_vocab)
-review_filter.fit_on_texts(review_pd.text.values.tolist())
-
-#split train and test data
-print "Splitting train-test data"
-testsize = int(len(review_pd)*.1)
-np.random.seed(9)
-review_pd = review_pd.sample(frac=1.)
-review_pd['numOccurences'] = 1
-review_pd['numBiz'] = review_pd.groupby('business_id')['numOccurences'].transform('cumsum')
-review_pd['numUser'] = review_pd.groupby('user_id')['numOccurences'].transform('cumsum')
-pd.crosstab(review_pd['numBiz'],review_pd['numUser'])
-test_index_pool = review_pd[(review_pd['numBiz']>1) & (review_pd['numUser']>1)].index
-
-
-test_index = np.random.choice(test_index_pool,size=testsize,replace=False)
-train_index = [x for x in review_pd.index if x not in test_index]
-
-testdf = review_pd.loc[test_index,:]
-traindf= review_pd.loc[train_index,:]
-
-#save both the filter and the PD as pickled python objects, and the vocab map.
-print "Dumping files to HDD..."
-with open(os.path.join(datadir,'yelp_train_data.p'),'w') as f:
-    cPickle.dump(traindf,f)
-with open(os.path.join(datadir,'yelp_test_data.p'),'w') as f:
-    cPickle.dump(testdf,f)
-with open(os.path.join(datadir,'yelp_textfilter.p'),'w') as f:
-    cPickle.dump(review_filter,f)
+del review_jsons
+#remove ambience indicators (except funny which we keep)
+reviews.drop(['cool','useful','date',],axis=1,inplace=True)
+reviews.drop(['review_id'],axis=1,inplace=True)
+gc.collect()
+reviews.head()
+print "Dumping..."
+with open(os.path.join(datadir,'yelp_data.p'),'w') as f:
+    cPickle.dump(reviews,f)
+#
--- paper/feat2vec_yelp.py
+++ paper/feat2vec_yelp.py
@@ -8,44 +8,64 @@ import sys
 sys.path.append('feat2vec/')
 import matplotlib.pyplot as plt
 import keras
+import gc
 from keras.preprocessing.text import Tokenizer
 from keras.layers import Input
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import feat2vec
 from feat2vec.feat2vec import Feat2Vec
-
-
+import yaml
+constants = yaml.load(open("paper/constants.yaml", 'rU'))
+datadir =constants['datadir']
+glovedir=constants['wordembeddir']
+#hyperparameters
+batch_size=1000
+feature_alpha=0.25
+sampling_alpha=.75
+negative_samples=5
+dim = 50
+text_embedding_size=50
+text_max_len = 250
 
 ### Load DF
-datadir = '/home/luis/Data/Yelp/dataset/'
 print "Loading DF..."
-with open(os.path.join(datadir,'yelp_train_data.p'),'r') as f:
+#with open(os.path.join(datadir,'yelp_train_data.p'),'r') as f:
+#    traindf=cPickle.load(f)
+#with open(os.path.join(datadir,'yelp_train_mini.p'),'w') as f:
+#    cPickle.dump(traindf.iloc[0:10000],f)
+with open(os.path.join(datadir,'yelp_train_mini.p'),'r') as f:
     traindf=cPickle.load(f)
 with open(os.path.join(datadir,'yelp_textfilter.p'),'r') as f:
     review_filter = cPickle.load(f)
 
 vocab_map = {}
-for c in ['business_id','user_id','stars']:
+for c in ['business_id','user_id','stars','funny']:
     vocab_map[c] = dict([(cat,i) for i,cat in enumerate(traindf[c].cat.categories)])
     traindf[c] = traindf[c].cat.codes
 
+print traindf.head()
+vocab_map['textseq'] = review_filter.word_index
 #move the star var to 0-1
 # for c in ['stars']:
 #     vocab_map[c] = {'max':np.max(traindf[c]),'min':np.min(traindf[c])}
 #     print c,vocab_map[c]
 #     traindf[c] = ( traindf[c] - np.min(traindf[c]) ) / (np.max(traindf[c]) - np.min(traindf[c]) )
 
-vocab_map['textseq'] = review_filter.word_index
 #traindf = traindf.iloc[0:100000,:]
 
-### Hyperparameters
-batch_size=2000
-feature_alpha=0.5
-sampling_alpha=.75
-negative_samples=5
-dim = 50
-text_embedding_size=25
-text_max_len = 250
+glovefile =os.path.join(glovedir,'glove.6B.{d}d.txt'.format(d=text_embedding_size))
+glovevecs = np.zeros(shape=(review_filter.num_words,text_embedding_size))
+numwords =0
+with open(glovefile,'r') as f:
+    for line in f:
+        splitLine = line.split()
+        word = splitLine[0].lower()
+        if word in vocab_map['textseq']:
+            idx = vocab_map['textseq'][word]
+            if idx < review_filter.num_words:
+                numwords+=1
+                embedding = np.array([float(val) for val in splitLine[1:]])
+                glovevecs[idx,:] = embedding
 
 ### Filter text
 #review_pd['textseq'] = review_filter.texts_to_sequences(review_pd.text.values.tolist())
@@ -53,18 +73,20 @@ text_max_len = 250
 #review_pd['textseq'] = [textmat[i,:] for i in range(len(review_pd))]
 #num_words = len(review_filter.word_index) +1
 #print num_words
+
+### Hyperparameters
+
+print "Creating Model..."
 def process_keras_text(df):
     #seq = review_filter.texts_to_sequences(df.textseq.values.tolist())
     mat = keras.preprocessing.sequence.pad_sequences(df.textseq.values.tolist(),maxlen=text_max_len,value=0,padding='post',truncating='post')
     return mat
-
-print "creating sequences of text"
-traindf['textseq'] = review_filter.texts_to_sequences(traindf.text.values.tolist())
-traindf['textseq'] = traindf['textseq'].map(lambda x: tuple(x))
 #### Build NLP keras layer
+import tensorflow
+reload(keras)
 text_input = Input(batch_shape=(None, text_max_len), name='wordind_seq')
 word_embed_layer=keras.layers.Embedding(input_dim=review_filter.num_words, output_dim=text_embedding_size,
-    input_length=text_max_len, mask_zero=False, name='word_embedding_layer')(text_input)
+    input_length=text_max_len, weights=[glovevecs],mask_zero=False, name='word_embedding_layer')(text_input)
 conv_layer=keras.layers.convolutional.Convolution1D(filters=100,
     kernel_size=3,activation='relu',name='conv')(word_embed_layer)
 maxpool_layer=keras.layers.GlobalMaxPool1D()(conv_layer)
@@ -75,15 +97,17 @@ text_embed_layer=keras.layers.Dense(dim,name='embedding_textseq',activation='lin
 # star_embed =  keras.layers.Dense(dim,name='embedding_stars',activation='linear')(star_deep)
 
 ### define F2V params
-model_features = [['business_id'],['user_id'],['stars'],['textseq']]
-is_deepin_feature=[False,False,False,True]
-realvalued=[False,False,False,False]
-model_feature_names = ['business_id','user_id','stars', 'textseq']
+model_features = [['business_id'],['user_id'],['stars'],['funny'],['textseq']]
+is_deepin_feature=[False,False,False,False,True]
+realvalued=[False,False,False,False,False]
+model_feature_names = ['business_id','user_id','stars','funny', 'textseq']
 feature_dimensions = [ len(vocab_map['business_id'].keys()),
                        len(vocab_map['user_id'].keys()),
-                       len(vocab_map['stars'].keys()),None]
+                       len(vocab_map['stars'].keys()),
+                       len(vocab_map['funny'].keys()),
+                       None]
 sampling_features = [f for f in model_features]
-custom_formats=[None,None,None,process_keras_text]
+custom_formats=[None,None,None,None,process_keras_text]
 #deep_input_layers=[star_input,text_input]
 #deepin_layers = [star_embed,text_embed_layer]
 deep_input_layers=[text_input]
@@ -111,17 +135,27 @@ f2v = Feat2Vec(df=traindf,model_feature_names=model_feature_names,
     negative_samples=negative_samples,
     sampling_bias=0,batch_size=batch_size)
 
-print f2v.model.summary()
+
 earlyend = EarlyStopping(patience=0,monitor='val_loss')
 chkpoint = ModelCheckpoint(filepath=os.path.join(datadir,'checkpoint.h5'),monitor='val_loss',
     verbose=1,save_best_only=True,save_weights_only=True)
-callbacks=[earlyend,chkpoint]
+logger=keras.callbacks.CSVLogger(os.path.join(datadir,'logs.csv'))
+
+callbacks=[earlyend,chkpoint,logger]
+#save weights for re-initalization
+initial_weights = f2v.model.get_weights()
+
 f2v.fit_model(epochs=25,validation_split=1./9.,
     callbacks = callbacks)
-f2v.model.load_weights(os.path.join(datadir,'checkpoint.h5'))
 
+#now determine optimal number of epochs, and train the full sample with these.
+opt_epoch = len(f2v.model.history.epoch) - 1
+print "Optimal Epochs: ", opt_epoch
+f2v.model.set_weights(initial_weights)
+f2v.fit_model(epochs=opt_epoch,validation_split=None)
 
 #save learned embeddings from model
+print "saving embeddings/model..."
 embeddings = f2v.get_embeddings(vocab_map)
 print embeddings
 embeddings.to_csv(os.path.join(datadir,'yelp_embeddings.tsv'),sep='\t',index=False)
