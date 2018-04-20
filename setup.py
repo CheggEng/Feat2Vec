666d5b3f047596662e226e22c7a13d800c8d1955
José González-Brenes <jgonzalez@chegg.com>
2018-04-20 13:19:46 -0700

    adding constraints for deepout parameters

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='feat2vec',
-      version='0.2.2',
+      version='0.3.0',
       description='Feat2Vec Library',
       author='Anonymous Author for Peer Review',
       packages=[
